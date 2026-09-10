b2bb8d29b3fd8c0ef2d973105993be20bc722def
SebastianFlores98 <83105821+SebastianFlores98@users.noreply.github.com>
2026-09-10 14:48:11 -0300

    Clases Dominio y Negocio
    - Agregue las clases de Marca y Imagen a Dominio
    - Borre las clases por defecto de Dominio y negocio

--- TPWinForm_equipo-q/Dominio/Marca.cs
+++ TPWinForm_equipo-q/Dominio/Marca.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Class1
+    public class Marca
     {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
     }
+
 }
+    
