f3590fcbbfa7de99262c240e206a74a3420a9055
Soledad Córdoba <cordoba.soledad@hotmail.com>
2026-09-13 12:02:51 -0300

    Conexión a BD

---
+++ TPWinForm_equipo-q/Datos/ConexionDatos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class ConexionDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public ConexionDatos()
+        {
+            // Conexión a base
+            /* 
+             * NOTA: Para desarrollo local usamos SQL Server en Docker (puerto 1433).
+             * Si se ejecuta con SQL Server Express local, cambiar a: "server=.\SQLEXPRESS; ..."
+            */
+            conexion = new SqlConnection("Server=localhost,1433;Database=CATALOGO_P3_DB;User Id=sa;Password=BaseDatos2!;TrustServerCertificate=True;");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
