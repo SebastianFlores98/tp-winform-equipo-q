173871c9656a515a6ab20d134b7d1dd3b81f1d5c
Soledad Córdoba <cordoba.soledad@hotmail.com>
2026-09-13 20:39:35 -0300

    Cambio la clase Articulo a Public

--- TPWinForm_equipo-q/Dominio/Articulo.cs
+++ TPWinForm_equipo-q/Dominio/Articulo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    internal class Articulo
+    public class Articulo
     {
         public string CodigoArticulo { get; set; }
         public string Nombre { get; set; }
